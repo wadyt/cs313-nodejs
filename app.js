e4f3415f79399396cc6ef8baa51cb2e0dc972687
wadyt <wady.tupiza@gmail.com>
2017-06-16 20:06:08 -0500

    Ejs created

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var express = require('express'),
     service = require('./service.js'),
     server = require('http').createServer(app);
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
     extended: true
-    }));
+}));
 
-    service.attachService(app);
-    server.listen(process.env.PORT || 5000);
+service.attachService(app);
+server.listen(process.env.PORT || 5000);
 
-    app.get('/', function(req, res){
-        res.sendFile(__dirname + '/index.html');
-    });
-    console.log("server running at http://localhost:5000");
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+console.log("server running at http://localhost:5000");
